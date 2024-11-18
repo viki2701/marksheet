8e50a1f61c4c5500fe0fa8cf495da7e58d9cfb60
viki2701 <viki2727eee@gmail.com>
2024-11-18 16:09:26 +0530

    loging screen

--- mark/src/app/app.routes.ts
+++ mark/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { CreateuserComponent } from './createuser/createuser.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'home',
+        component:HomeComponent,
+    },
+    {
+        path:'creatuser',
+        component:CreateuserComponent,
+      },
+      {
+        path:'login',
+        component:LoginComponent,
+      },
+      {
+        path:'',
+        redirectTo:'home',
+        pathMatch:'full'
+      },
+      {
+        path:'**',
+        redirectTo:'home',
+        pathMatch:'full'
+      }
+];
---
+++ mark/src/app/createuser/createuser.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createuser',
+  standalone: true,
+  imports: [],
+  templateUrl: './createuser.component.html',
+  styleUrl: './createuser.component.scss'
+})
+export class CreateuserComponent {
+
+}
